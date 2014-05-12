285194694f47060ba5f712b625038dd8d5cd5853
eletendre <eletendre15@smpanthers.org>
2014-05-12 14:05:27 -0700

    project

---
+++ code.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ $("button").click(function() {
+  $("#gooj").fadeOut("slow");
+ });
+});
+
+$(document).ready(function() {
+ $("button").click(function() {
+  $("#woo").fadeOut("slow");
+ });
+});
